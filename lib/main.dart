10bc5ab5119b3f17db359987c152f418d1c550d7
ariqfrhan <muhammadariq3@gmail.com>
2023-12-28 00:58:43 +0700

    `feat` : listview x builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,30 +7,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
-  final List<Widget> myList = [
-          Container(
-            height: 300,
-            width: 3000,
-            color: Colors.red,
-          ),
-          Container(
-            height: 300,
-            width: 3000,
-            color: Colors.green,
-          ),
-          Container(
-            height: 300,
-            width: 3000,
-            color: Colors.blue,
-          ),
-          Container(
-            height: 300,
-            width: 3000,
-            color: Colors.yellow,
-          )
-        ];
-
-    final List<Color> myColor = [Colors.red, Colors.green, Colors.blue, Colors.yellow];
+  var faker = Faker();
 
 
   @override
@@ -39,23 +17,58 @@ class MyApp extends StatelessWidget {
       home: Scaffold(appBar: AppBar(
         title: const Text("List View"),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            contentPadding: EdgeInsets.all(10),
-            title: Text('Ariq'),
-            subtitle: Text('Lorem ipsum sor dolor ameta daosdaodsmaosdaosdoasodmaomsdaosdoasodamdoasmda',
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,),
-            leading: CircleAvatar(),
-            trailing: Text('10.00'),
-            onTap: (){
-              return;
-            },
-          )
-        ],
-      )
+      // body: ListView(
+      //   children: [
+      //     ChatItem(imageUrl: 'https://picsum.photos/id/1/200/300',
+      //     title: faker.person.name(),
+      //     subtitle: faker.lorem.sentence()),
+      //     ChatItem(imageUrl: 'https://picsum.photos/id/1/200/300',
+      //     title: faker.person.name(),
+      //     subtitle: faker.lorem.sentence()),
+      //     ChatItem(imageUrl: 'https://picsum.photos/id/1/200/300',
+      //     title: faker.person.name(),
+      //     subtitle: faker.lorem.sentence()),
+      //     ChatItem(imageUrl: 'https://picsum.photos/id/1/200/300',
+      //     title: faker.person.name(),
+      //     subtitle: faker.lorem.sentence())
+      //   ],
+      // )
+      body: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index){
+          return ChatItem(imageUrl: 'https://picsum.photos/id/1/200/300', title: faker.person.name(), subtitle: faker.lorem.sentence());
+        }
+        ),
       ),
+    );
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String subtitle;
+
+  const ChatItem({
+    super.key,
+    required this.imageUrl, required this.title, required this.subtitle
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.all(10),
+      title: Text(title),
+      subtitle: Text(subtitle,
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      trailing: Text('10.00'),
+      onTap: (){
+        return;
+      },
     );
   }
 }
